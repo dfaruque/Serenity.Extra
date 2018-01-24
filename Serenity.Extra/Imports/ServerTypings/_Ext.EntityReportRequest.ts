9767465cc4b2588bf3e2ba924eaf6401acf579a9
dfaruque <dfaruque@gmail.com>
2018-01-24 11:51:23 +0600

    deleted unneccessary files

--- Serenity.Extra/Imports/ServerTypings/_Ext.EntityReportRequest.ts
+++
@@ -1,8 +0,0 @@
-﻿namespace _Ext {
-    export interface EntityReportRequest extends Serenity.RetrieveRequest {
-        ReportKey?: string;
-        ReportServiceMethodName?: string;
-        ReportDesignPath?: string;
-    }
-}
-
