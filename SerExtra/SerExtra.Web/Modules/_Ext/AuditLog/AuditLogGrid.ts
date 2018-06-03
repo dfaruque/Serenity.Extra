ad1f452f80348d7bfc2851401a10d87687ee489d
dfaruque <dfaruque@gmail.com>
2018-06-03 12:32:51 +0600

    hide add button in AuditLogGrid

--- SerExtra/SerExtra.Web/Modules/_Ext/AuditLog/AuditLogGrid.ts
+++ SerExtra/SerExtra.Web/Modules/_Ext/AuditLog/AuditLogGrid.ts
@@ -3,7 +3,7 @@
 namespace _Ext {
 
     @Serenity.Decorators.registerClass()
-    export class AuditLogGrid extends _Ext.GridBase<AuditLogRow, any> {
+    export class AuditLogGrid extends GridBase<AuditLogRow, any> {
         protected getColumnsKey() { return '_Ext.AuditLog'; }
         protected getDialogType() { return AuditLogDialog; }
         protected getIdProperty() { return AuditLogRow.idProperty; }
@@ -13,7 +13,7 @@ namespace _Ext {
         constructor(container: JQuery) {
             super(container);
         }
-
+        
         protected getButtons() {
             var buttons = super.getButtons();
             buttons.splice(0, 1);
