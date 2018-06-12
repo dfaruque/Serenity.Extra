034ec5f9e4181ba3f523382759277abb30654eb7
dfaruque <dfaruque@gmail.com>
2018-06-12 11:41:24 +0600

    added HardCodedLookupEditor

---
+++ SerExtra/SerExtra.Web/Modules/_Ext/Editors/HardCodedLookupEditor.ts
@@ -0,0 +1,31 @@
+﻿namespace _Ext {
+
+    @Serenity.Decorators.registerEditor()
+    export class HardCodedLookupEditor extends Serenity.Select2Editor<any, any> {
+
+        constructor(container: JQuery, options: HardCodedLookupEditorOptions) {
+            super(container, options);
+
+            let source = options.sourceArray;
+            if (options.sourceCSV) {
+                source = options.sourceCSV.split(',');
+            }
+
+            source.forEach(i => this.addOption(i, i));
+        }
+
+        protected getSelect2Options() {
+            let opt = super.getSelect2Options();
+
+
+
+            return opt;
+        }
+    }
+
+    export interface HardCodedLookupEditorOptions {
+        sourceArray: string[];
+        sourceCSV: string;
+        allowOtherValue: boolean;
+    }
+}
