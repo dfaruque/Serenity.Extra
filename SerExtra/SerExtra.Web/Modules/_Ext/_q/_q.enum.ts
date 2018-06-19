99023db6571c74a8d19987f85f392d8edebb6955
dfaruque <dfaruque@gmail.com>
2018-06-19 18:55:11 +0600

    added more enum unils

--- SerExtra/SerExtra.Web/Modules/_Ext/_q/_q.enum.ts
+++ SerExtra/SerExtra.Web/Modules/_Ext/_q/_q.enum.ts
@@ -6,4 +6,26 @@ namespace q {
         return title;
     }
 
+    export function getEnumValues(enumType): string[] {
+        let items: string[] = [];
+        for (let item in enumType) {
+            if (!isNaN(Number(item))) {
+                items.push(enumType[item]);
+            }
+        }
+
+        return items;
+    }
+
+    export function getEnumKeys(enumType): string[] {
+        let items: string[] = [];
+        for (let item in enumType) {
+            if (!isNaN(Number(item))) {
+                items.push(item);
+            }
+        }
+
+        return items;
+    }
+
 }
