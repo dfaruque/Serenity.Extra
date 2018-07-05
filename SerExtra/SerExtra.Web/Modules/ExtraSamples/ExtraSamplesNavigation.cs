7f7e4cc43ef370f7d4147058a23cb4b334f6d004
dfaruque <dfaruque@gmail.com>
2018-07-05 18:15:59 +0600

    fix report nav

--- SerExtra/SerExtra.Web/Modules/ExtraSamples/ExtraSamplesNavigation.cs
+++ SerExtra/SerExtra.Web/Modules/ExtraSamples/ExtraSamplesNavigation.cs
@@ -2,4 +2,4 @@
 using SerExtra.ExtraSamples.Pages;
 
 [assembly: NavigationMenu(7900, "Extra Samples", icon: "fa-plus")]
-[assembly: NavigationLink(7901, "Extra Samples/Orders By Castomer", typeof(OrdersByCustomerController))]
+[assembly: NavigationLink(7901, "Extra Samples/Reports/Orders By Castomer", typeof(OrdersByCustomerController))]
