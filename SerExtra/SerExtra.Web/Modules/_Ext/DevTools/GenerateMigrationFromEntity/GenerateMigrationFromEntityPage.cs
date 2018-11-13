fa5261a143bfd64e78b6e53431b387b76fa21077
dfaruque <dfaruque@gmail.com>
2018-11-13 11:03:00 +0600

    Generate Migration from Entity (beta)

--- SerExtra/SerExtra.Web/Modules/_Ext/DevTools/GenerateMigrationFromEntity/GenerateMigrationFromEntityPage.cs
+++ SerExtra/SerExtra.Web/Modules/_Ext/DevTools/GenerateMigrationFromEntity/GenerateMigrationFromEntityPage.cs
@@ -9,6 +9,8 @@ namespace _Ext.DevTools.Pages
     using System.Reflection;
     using Serenity.Data.Mapping;
     using Serenity.Web;
+    using System.IO;
+    using System.Web.Hosting;
 
     [RoutePrefix("GenerateMigrationFromEntity"), Route("{action=index}")]
     [PageAuthorize("DevTools:GenerateMigrationFromEntity")]
@@ -17,9 +19,76 @@ namespace _Ext.DevTools.Pages
         [Authorize, HttpGet]
         public ActionResult Index()
         {
+            return View("~/Modules/_Ext/DevTools/GenerateMigrationFromEntity/GenerateMigrationFromEntityIndex.cshtml");
+        }
+
+        [Authorize, HttpGet]
+        public ActionResult MigrationViewer()
+        {
             var model = new Model.GenerateMigrationFromEntityPageModel();
 
-            return View("~/Modules/_Ext/DevTools/GenerateMigrationFromEntity/GenerateMigrationFromEntityIndex.cshtml", model);
+            return View("~/Modules/_Ext/DevTools/GenerateMigrationFromEntity/GenerateMigrationFromEntityMigrationViewer.cshtml", model);
+        }
+
+        public ActionResult GenerateSingleFile()
+        {
+            var model = new Model.GenerateMigrationFromEntityPageModel();
+
+            ////write a cs file
+            var filePath = HostingEnvironment.MapPath("~/App_Data/GeneratedMigrations/InitialMigration.cs");
+            Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+            System.IO.File.Delete(filePath);
+            foreach (var migration in model.Migrations)
+            {
+                if (!string.IsNullOrWhiteSpace(migration.Remarks))
+                    System.IO.File.AppendAllText(filePath, migration.Remarks + Environment.NewLine);
+                System.IO.File.AppendAllText(filePath, migration.Migration);
+            }
+
+
+            return View("~/Modules/_Ext/DevTools/GenerateMigrationFromEntity/GenerateMigrationFromEntitySuccessMsg.cshtml", model);
+        }
+
+        public ActionResult GenerateFileForEachTable()
+        {
+            var model = new Model.GenerateMigrationFromEntityPageModel();
+
+            foreach (var migration in model.Migrations)
+            {
+                ////write a cs file
+                var migrationFileName = $"{migration.PaddedMigrationNumber}_{migration.TableName}";
+                var filePath = HostingEnvironment.MapPath($"~/App_Data/GeneratedMigrations/{migrationFileName}.cs");
+                Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                System.IO.File.Delete(filePath);
+                if (!string.IsNullOrWhiteSpace(migration.Remarks))
+                    System.IO.File.AppendAllText(filePath, migration.Remarks + Environment.NewLine);
+
+                string migrationClass = $@"using FluentMigrator;
+
+namespace DBMigration.Migrations
+{{
+
+    [Migration({migration.PaddedMigrationNumber})]
+    public class {migration.TableName} : Migration
+    {{
+        public override void Up()
+        {{
+           {migration.Migration}
+        }}
+
+        public override void Down()
+        {{
+            Delete.Table(""{migration.TableName}"");
+        }}
+    }}
+
+}}";
+
+                System.IO.File.AppendAllText(filePath, migrationClass);
+            }
+
+
+            return View("~/Modules/_Ext/DevTools/GenerateMigrationFromEntity/GenerateMigrationFromEntitySuccessMsg.cshtml", model);
         }
     }
 }
--- SerExtra/SerExtra.Web/Modules/_Ext/DevTools/GenerateMigrationFromEntity/GenerateMigrationFromEntityPageModel.cs
+++ SerExtra/SerExtra.Web/Modules/_Ext/DevTools/GenerateMigrationFromEntity/GenerateMigrationFromEntityPageModel.cs
@@ -5,15 +5,17 @@ using Serenity.Data.Mapping;
 using Serenity.Web;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Web.Hosting;
 
 namespace _Ext.DevTools.Model
 {
     public class GenerateMigrationFromEntityPageModel
     {
-        public List<string> Migrations { get; set; } = new List<string>();
+        public List<MigrationModel> Migrations { get; set; } = new List<MigrationModel>();
 
         public GenerateMigrationFromEntityPageModel()
         {
@@ -21,12 +23,23 @@ namespace _Ext.DevTools.Model
 
             var rowClasses = assembly.GetTypes().Where(w => w.GetCustomAttribute<ConnectionKeyAttribute>() != null && w.IsSealed);
 
+            var migrationNumber = 0;
             foreach (var rowClass in rowClasses)
             {
                 Row row = (Row)Activator.CreateInstance(rowClass);
 
+
                 string tableName = SchemaHelper.GetTableNameOnly(row.Table);
                 string schema = SchemaHelper.GetSchemaName(row.Table);
+                migrationNumber += 10;
+
+                var migrationModel = new MigrationModel
+                {
+                    RowType = rowClass,
+                    SchemaName = schema,
+                    TableName = tableName,
+                    MigrationNumber = migrationNumber
+                };
 
                 var rowFields = row.GetFields();
 
@@ -45,7 +58,7 @@ namespace _Ext.DevTools.Model
                         string strSize = rowfield.Type == FieldType.String ? rowfield.Size.ToString() : "";
 
                         sb.AppendLine();
-                        sb.Append("<br />");
+                        //sb.Append("<br />");
                         sb.Append($@".WithColumn(""{rowfield.Name}"")");
 
                         if (rowfield.Type == FieldType.String)
@@ -65,10 +78,14 @@ namespace _Ext.DevTools.Model
 
                         if (!string.IsNullOrWhiteSpace(rowfield.ForeignTable))
                         {
+
                             string foreignTableName = SchemaHelper.GetTableNameOnly(rowfield.ForeignTable);
+                            string foreignSchema = SchemaHelper.GetSchemaName(rowfield.ForeignTable);
+
+                            migrationModel.ParentTables.Add(foreignTableName);
+
                             sb.Append($@".ForeignKey(""{foreignTableName}"", ""{rowfield.ForeignField}"")");
 
-                            string foreignSchema = SchemaHelper.GetSchemaName(rowfield.ForeignTable);
                             if (!string.IsNullOrWhiteSpace(foreignSchema) && foreignSchema != "dbo")
                                 sb.Append($@".InSchema(""{schema}"")");
                         }
@@ -77,11 +94,98 @@ namespace _Ext.DevTools.Model
 
                 sb.Append(";");
                 sb.AppendLine();
-                sb.Append("<br />&nbsp;");
+                sb.AppendLine();
 
-                Migrations.Add(sb.ToString());
+                migrationModel.Migration = sb.ToString();
+                if (Migrations.Exists(e => e.TableName == migrationModel.TableName))
+                {
+                    migrationModel.Remarks += $"//Warning: Duplicate migration found." + Environment.NewLine
+                        + $"//TableName: {migrationModel.TableName}." + Environment.NewLine
+                        + $"//RowClass: {migrationModel.RowType.FullName}" + Environment.NewLine;
+
+                }
+
+                Migrations.Add(migrationModel);
+            }
+
+
+            //gather parent migrations
+            foreach (var migration in Migrations)
+            {
+                for (int i = 0; i < migration.ParentTables.Count - 1; i++)
+                {
+                    var parentTable = migration.ParentTables[i];
+
+                    var parentMigration = Migrations.Find(f => f.TableName == parentTable);
+                    if (parentMigration != null)
+                    {
+                        migration.ParentTablesMigrations.Add(parentMigration);
+                    }
+                    else
+                    {
+                        migration.Remarks += $"//Warning: {parentTable} has been used as Foreign Table but the corresponding Entity/Row is not found." + Environment.NewLine;
+                    }
+
+                    if (migration.MigrationNumber <= migration.MaxParentMigrationNumber)
+                        migration.MigrationNumber = migration.MaxParentMigrationNumber + 1;
+                }
 
             }
+
+            //ordering migrations accroding to dependancies
+            int arrangementCount = 0; //safety counter
+            bool needArrangement = true;
+            while (needArrangement && arrangementCount < 10000)
+            {
+                arrangementCount++;
+
+                var notArrangedMigrations = Migrations.Where(w => w.IsArranged == false);
+                foreach (var migration in notArrangedMigrations)
+                {
+                    var maxNumber = migration.MaxParentMigrationNumber;
+
+                    //while (true)
+                    //{
+                    //    var parentMigrations = migration.ParentTablesMigrations;
+                    //    if (parentMigrations.Count == 0) break;
+                    //    foreach (var parentMigration in parentMigrations)
+                    //    {
+                    //        if (maxNumber < parentMigration.MaxParentMigrationNumber)
+                    //            maxNumber = parentMigration.MaxParentMigrationNumber;
+
+                    //    }
+
+                    //}
+
+                    if (migration.MigrationNumber < maxNumber)
+                        migration.MigrationNumber = maxNumber + 2;
+                }
+
+                needArrangement = Migrations.Exists(w => w.IsArranged == false);
+            }
+
+
+            Migrations = Migrations.OrderBy(o => o.MigrationNumber).ToList();
+
         }
+    }
+
+    public class MigrationModel
+    {
+
+        public long MigrationNumber { get; set; } = 1;
+        public string PaddedMigrationNumber => "1" + MigrationNumber.ToString().PadLeft(6, '0');
+        public Type RowType { get; set; }
+        public string SchemaName { get; set; }
+        public string TableName { get; set; }
+        public List<string> ParentTables { get; set; } = new List<string>();
+        public List<MigrationModel> ParentTablesMigrations { get; set; } = new List<MigrationModel>();
+        public List<MigrationModel> ChildTablesMigrations { get; set; } = new List<MigrationModel>();
+        public string Migration { get; set; }
+        public string Remarks { get; set; } = string.Empty;
+
+        public bool IsArranged => MigrationNumber >= MaxParentMigrationNumber;
+
+        public long MaxParentMigrationNumber => ParentTablesMigrations.Count > 0 ? ParentTablesMigrations.Max(m => m.MigrationNumber) : 0;
     }
 }
