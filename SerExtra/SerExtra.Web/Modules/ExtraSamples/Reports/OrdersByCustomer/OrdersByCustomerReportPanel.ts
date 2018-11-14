49cda5b8709ef255c27568efabc75dbf9c494b62
dfaruque <dfaruque@gmail.com>
2018-11-14 14:12:14 +0600

    missing namespace

--- SerExtra/SerExtra.Web/Modules/ExtraSamples/Reports/OrdersByCustomer/OrdersByCustomerReportPanel.ts
+++ SerExtra/SerExtra.Web/Modules/ExtraSamples/Reports/OrdersByCustomer/OrdersByCustomerReportPanel.ts
@@ -1,7 +1,7 @@
 ﻿namespace SerExtra.ExtraSamples {
 
     @Serenity.Decorators.registerClass()
-    export class OrdersByCustomerReportPanel extends ReportPanelBase<OrdersByCustomerReportRequest> {
+    export class OrdersByCustomerReportPanel extends _Ext.ReportPanelBase<OrdersByCustomerReportRequest> {
         protected getReportTitle() { return 'Orders By Customer'; }
         protected getReportKey() { return 'ExtraSamples.OrdersByCustomerReport'; }
         protected getFormKey() { return OrdersByCustomerReportRequestForm.formKey; }
