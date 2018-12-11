84363bc179ede56a6d3d9f04c663f4e87df2e441
dfaruque <dfaruque@gmail.com>
2018-12-11 14:33:34 +0600

    DevTools nav icon

--- SerExtra/SerExtra.Web/Modules/_Ext/_ExtNavigation.cs
+++ SerExtra/SerExtra.Web/Modules/_Ext/_ExtNavigation.cs
@@ -1,7 +1,9 @@
 ﻿using Serenity.Navigation;
 using MyPages = _Ext.DevTools.Pages;
 
-[assembly: NavigationLink(int.MaxValue, "Administration/Audit Log", typeof(_Ext.Pages.AuditLogController), icon: null)]
+[assembly: NavigationLink(int.MaxValue, "Administration/Audit Log", typeof(_Ext.Pages.AuditLogController), icon: "fa-history")]
+
+[assembly: NavigationMenu(int.MaxValue, "DevTools", icon: "fa-code")]
 [assembly: NavigationLink(int.MaxValue, "DevTools/Compare Entity to DB", typeof(MyPages.CompareEntityToDBController), icon: null)]
 [assembly: NavigationLink(int.MaxValue, "DevTools/Generate Migrations from Entity", typeof(MyPages.GenerateMigrationFromEntityController), icon: null)]
 [assembly: NavigationLink(int.MaxValue, "DevTools/Sergen", typeof(MyPages.SergenController), icon: null)]
