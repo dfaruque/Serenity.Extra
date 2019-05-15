a34bdedd47806ab841a526398bd1f58b0153dbf6
dfaruque <dfaruque@gmail.com>
2019-05-15 11:39:22 +0600

    added MonthYearFormatter

---
+++ SerExtra/SerExtra.Web/Modules/_Ext/Formatters/MonthYearFormatter.ts
@@ -0,0 +1,18 @@
+﻿namespace _Ext {
+
+    @Serenity.Decorators.registerFormatter([Serenity.ISlickFormatter])
+    export class MonthYearFormatter implements Slick.Formatter {
+        static format(ctx: Slick.FormatterContext) {
+            if (ctx.value) {
+                let val = Q.parseISODateTime(ctx.value);
+
+                return Months[val.getMonth()] + '-' + val.getFullYear();
+            } else return '';
+        }
+
+        format(ctx: Slick.FormatterContext) {
+            return MonthYearFormatter.format(ctx);
+        }
+    }
+
+}
