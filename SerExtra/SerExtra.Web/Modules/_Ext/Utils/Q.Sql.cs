0773afeb914e597b16cbba53064e0d4f1c57a3ba
dfaruque <dfaruque@gmail.com>
2019-05-22 14:53:37 +0600

    added Q.Sql

---
+++ SerExtra/SerExtra.Web/Modules/_Ext/Utils/Q.Sql.cs
@@ -0,0 +1,21 @@
+﻿using EMIS;
+using EMIS.Navigation;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Text;
+using Serenity;
+using Serenity.ComponentModel;
+using Serenity.Data;
+using Serenity.Data.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+public static partial class Q
+{
+    public static string SqlRound(string fieldExp, int? precision = 2)
+    {
+        return $"ROUND({fieldExp}, {precision})";
+    }
+}
