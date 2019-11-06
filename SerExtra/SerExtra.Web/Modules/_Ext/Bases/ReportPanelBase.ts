e8eac9774d66e210074ab53886bc14133512de14
dfaruque <dfaruque@gmail.com>
2019-11-06 16:12:49 +0600

    fixed report panel design

--- SerExtra/SerExtra.Web/Modules/_Ext/Bases/ReportPanelBase.ts
+++ SerExtra/SerExtra.Web/Modules/_Ext/Bases/ReportPanelBase.ts
@@ -1,7 +1,7 @@
 ﻿namespace _Ext {
 
     export class ReportPanelBase<TRequest> extends Serenity.PropertyPanel<TRequest, any> {
-        protected getTemplateName() { return 'ReportPanel'; }
+        //protected getTemplateName() { return 'ReportPanel'; }
         protected getReportTitle() { return 'Report Title'; }
         protected getReportKey() { return 'Report.Key'; }
 
@@ -10,6 +10,7 @@
         constructor(container: JQuery) {
             super(container);
             this.byId('PanelTitle').text(this.getReportTitle());
+            this.element.addClass('s-DataGrid');
 
             this.byId('SubmitButton').click(e => {
                 e.preventDefault();
@@ -36,5 +37,24 @@
 
         }
 
+        protected getTemplate() {
+            return `
+    <div class="page-title grid-title" id="~_PanelTitle">Report Title</div>
+
+    <div class="s-Form flex-layout">
+        <form id="~_Form" action="">
+            <div class="fieldset ui-widget ui-widget-content ui-corner-all">
+                <div id="~_PropertyGrid"></div>
+                <div class="clear"></div>
+            </div>
+        </form>
+        <br />
+        <div class="buttons align-center">
+            <button id="~_SubmitButton" class="btn btn-primary"><i class="fa fa-search margin-r-5"></i> Show</button>
+            <button id="~_DownloadPdfButton" class="btn btn-default"><i class="fa fa-file-pdf-o margin-r-5"></i> Download PDF</button>
+        </div>
+    </div>
+`;
+        }
     }
 }
