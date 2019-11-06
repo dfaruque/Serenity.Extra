ececd6073fcff49b20d696ba73bcd96e1c49b69e
dfaruque <dfaruque@gmail.com>
2019-11-06 18:28:45 +0600

    added _references.ts file to ignore warning: used before its declaration

--- SerExtra/SerExtra.Web/Imports/_references.ts
+++ SerExtra/SerExtra.Web/Imports/_references.ts
@@ -1,6 +1,8 @@
 ﻿/// <reference path="../Scripts/typings/serenity/Serenity.CoreLib.d.ts" />
 /// <reference path="../Scripts/typings/jspdf/jspdf.autotable.d.ts" />
 
+/// <reference path="../Modules/_Ext/_q/_q.var.ts" />
+
 /// <reference path="../Modules/_Ext/Bases/GridBase.ts" />
 /// <reference path="../Modules/_Ext/Bases/ReportGridBase.ts" />
 /// <reference path="../Modules/_Ext/Bases/ReportPanelBase.ts" />
@@ -11,4 +13,5 @@
 /// <reference path="../Modules/_Ext/Editors/EditorDialogBase.ts" />
 /// <reference path="../Modules/_Ext/Editors/GridEditorBase.ts" />
 
+/// <reference path="../Modules/_Ext/Editors/JsonGridEditorBase.ts" />
 
