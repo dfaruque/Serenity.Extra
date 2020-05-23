5158342d000b236fda27d315b476f311d7b2d9b7
dfaruque <dfaruque@gmail.com>
2020-05-23 15:03:30 +0600

    added Q.Round2

--- SerExtra/SerExtra.Web/Modules/_Ext/Utils/Q.Math.cs
+++ SerExtra/SerExtra.Web/Modules/_Ext/Utils/Q.Math.cs
@@ -2,8 +2,13 @@
 
 public static partial class Q
 {
-    public static decimal Round(decimal? value, int decimals = 5, MidpointRounding mode = MidpointRounding.AwayFromZero)
+    public static decimal Round(decimal? value, int decimals = 0, MidpointRounding mode = MidpointRounding.AwayFromZero)
     {
         return Math.Round(value ?? 0, decimals, mode);
+    }
+
+    public static decimal Round2(decimal? value, MidpointRounding mode = MidpointRounding.AwayFromZero)
+    {
+        return Math.Round(value ?? 0, 2, mode);
     }
 }
