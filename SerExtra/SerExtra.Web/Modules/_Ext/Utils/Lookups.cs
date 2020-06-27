0241b961ec6ada82a312e30d7cfd66f7c2bd921a
dfaruque <dfaruque@gmail.com>
2020-06-27 12:11:35 +0600

    ...

--- SerExtra/SerExtra.Web/Modules/_Ext/Utils/Lookups.cs
+++ SerExtra/SerExtra.Web/Modules/_Ext/Utils/Lookups.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace _Ext //enums must have namespace otherwise it transforms to wrong typescript code
+namespace _Ext
 {
     [LookupScript("EmptyLookup", Permission = "*")]
     public class EmptyLookup : LookupScript
