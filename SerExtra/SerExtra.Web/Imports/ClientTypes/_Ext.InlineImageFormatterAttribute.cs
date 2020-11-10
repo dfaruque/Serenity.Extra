e1e9f826ff10368d40c721f8f1cedd55f91c13a6
dfaruque <dfaruque@gmail.com>
2020-11-10 18:14:21 +0600

    T4

---
+++ SerExtra/SerExtra.Web/Imports/ClientTypes/_Ext.InlineImageFormatterAttribute.cs
@@ -0,0 +1,38 @@
+﻿using Serenity;
+using Serenity.ComponentModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace _Ext
+{
+    public partial class InlineImageFormatterAttribute : CustomFormatterAttribute
+    {
+        public const string Key = "_Ext.InlineImageFormatter";
+
+        public InlineImageFormatterAttribute()
+            : base(Key)
+        {
+        }
+
+        public String DefaultImage
+        {
+            get { return GetOption<String>("defaultImage"); }
+            set { SetOption("defaultImage", value); }
+        }
+
+        public String FileProperty
+        {
+            get { return GetOption<String>("fileProperty"); }
+            set { SetOption("fileProperty", value); }
+        }
+
+        public Boolean Thumb
+        {
+            get { return GetOption<Boolean>("thumb"); }
+            set { SetOption("thumb", value); }
+        }
+    }
+}
+
