7e70ec213d69603f9ab6c4feb826fa29ff88dac8
dfaruque <dfaruque@gmail.com>
2020-11-10 18:12:11 +0600

    added YesNoFormatter

---
+++ SerExtra/SerExtra.Web/Modules/_Ext/Formatters/YesNoFormatter.ts
@@ -0,0 +1,16 @@
+﻿namespace _Ext {
+
+    @Serenity.Decorators.registerFormatter([Serenity.ISlickFormatter])
+    export class YesNoFormatter implements Slick.Formatter {
+        static format(val) {
+            let valAsBool = Boolean(val);
+
+            return valAsBool ? Q.text('Dialogs.YesButton') : Q.text('Dialogs.NoButton');
+        }
+
+        format(ctx: Slick.FormatterContext) {
+            return YesNoFormatter.format(ctx.value);
+        }
+    }
+
+}
