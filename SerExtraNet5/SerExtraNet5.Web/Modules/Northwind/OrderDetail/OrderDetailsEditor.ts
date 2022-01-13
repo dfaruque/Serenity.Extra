02b0f074b5517f16e9ff83977cb2698c2ac1293b
dfaruque <dfaruque@gmail.com>
2022-01-13 17:21:11 +0600

    barcode input sample

--- SerExtraNet5/SerExtraNet5.Web/Modules/Northwind/OrderDetail/OrderDetailsEditor.ts
+++ SerExtraNet5/SerExtraNet5.Web/Modules/Northwind/OrderDetail/OrderDetailsEditor.ts
@@ -10,6 +10,40 @@ namespace SerExtraNet5.Northwind {
 
         constructor(container: JQuery) {
             super(container);
+
+            let $barcodeInput = $('<input type="text" />');
+
+            $barcodeInput.appendTo(this.toolbar.element)
+                .keypress(event => {
+                    if (event.keyCode == 13) {
+                        let productId = $barcodeInput.val();
+
+                        if (productId) {
+                            let eqFilter = {};
+                            eqFilter[ProductRow.Fields.ProductID] = productId
+
+                            ProductService.List({ EqualityFilter: eqFilter, Take: 1 }, response => {
+                                if (response.Entities.length > 0) {
+                                    let pickedProduct = response.Entities[0];
+
+                                    let orderDetails = this.value;
+                                    orderDetails.push({
+                                        ProductID: pickedProduct.ProductID,
+                                        ProductName: pickedProduct.ProductName,
+                                        UnitPrice: pickedProduct.UnitPrice,
+                                        Quantity: 1,
+                                        LineTotal: 1 * (pickedProduct.UnitPrice || 0)
+                                    });
+
+                                    this.value = orderDetails;
+                                } else {
+                                    Q.alert('Product not found!')
+                                }
+                            });
+                        }
+                    }
+                });
+
         }
 
         validateEntity(row, id) {
