d02bcc15afc28c3688f7cf18c66291bf1afd6e7c
dfaruque <dfaruque@gmail.com>
2022-04-06 14:11:31 +0600

    id should be withing 36 char (guid)

--- SerExtraNet5/SerExtraNet5.Web/Migrations/DefaultDB/DefaultDB_20180530_140000_AuditLog.cs
+++ SerExtraNet5/SerExtraNet5.Web/Migrations/DefaultDB/DefaultDB_20180530_140000_AuditLog.cs
@@ -8,11 +8,11 @@ namespace SerExtraNet5.Migrations.DefaultDB
         public override void Up()
         {
             this.CreateTableWithId64("AuditLog", "Id", s => s
-                .WithColumn("UserId").AsAnsiString().NotNullable()
+                .WithColumn("UserId").AsAnsiString(36).NotNullable()
                 .WithColumn("ActionType").AsInt32().NotNullable()
                 .WithColumn("ActionDate").AsDateTime().NotNullable()
                 .WithColumn("TableName").AsString(100).NotNullable()
-                .WithColumn("EntityId").AsAnsiString().NotNullable()
+                .WithColumn("EntityId").AsAnsiString(36).NotNullable()
                 .WithColumn("Changes").AsString(int.MaxValue).Nullable()
                 .WithColumn("IpAddress").AsString(100).Nullable()
                 .WithColumn("SessionId").AsString(100).Nullable()
