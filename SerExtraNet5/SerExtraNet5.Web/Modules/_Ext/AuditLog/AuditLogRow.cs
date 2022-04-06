cb4d64657c06b1964e9ab57823d79c32d2edce39
dfaruque <dfaruque@gmail.com>
2022-04-06 16:14:52 +0600

    refactored with new syntax

--- SerExtraNet5/SerExtraNet5.Web/Modules/_Ext/AuditLog/AuditLogRow.cs
+++ SerExtraNet5/SerExtraNet5.Web/Modules/_Ext/AuditLog/AuditLogRow.cs
@@ -1,13 +1,11 @@
 ﻿//DFA
 namespace _Ext.Entities
 {
-    using Serenity;
+    using System;
+    using System.ComponentModel;
     using Serenity.ComponentModel;
     using Serenity.Data;
     using Serenity.Data.Mapping;
-    using System;
-    using System.ComponentModel;
-    using System.IO;
 
     [ConnectionKey("Log"), TableName("[AuditLog]")]
     [DisplayName("Audit Log"), InstanceName("Audit Log"), TwoLevelCached]
@@ -16,58 +14,49 @@ namespace _Ext.Entities
     public sealed class AuditLogRow : Row<AuditLogRow.RowFields>, IIdRow, INameRow
     {
         [DisplayName("Id"), Identity, NotNull, IdProperty]
-        public Int64? Id { get { return Fields.Id[this]; } set { Fields.Id[this] = value; } }
+        public Int64? Id { get => Fields.Id[this]; set => Fields.Id[this] = value; }
 
         [DisplayName("User"), NotNull, QuickFilter]
         [LookupEditor("Administration.User")]
-        public String UserId { get { return Fields.UserId[this]; } set { Fields.UserId[this] = value; } }
+        public String UserId { get => Fields.UserId[this]; set => Fields.UserId[this] = value; }
 
         [DisplayName("Action Type"), NotNull, QuickFilter]
         [AuditLogActionTypeFormatter]
-        public AuditActionType? ActionType { get { return (AuditActionType?)Fields.ActionType[this]; } set { Fields.ActionType[this] = (int?)value; } }
+        public AuditActionType? ActionType { get => Fields.ActionType[this]; set => Fields.ActionType[this] = value; }
 
         [DisplayName("Action Date"), NotNull, QuickFilter]
         [DateTimeEditor, DateTimeFiltering, DateTimeFormatter]
-        public DateTime? ActionDate { get { return Fields.ActionDate[this]; } set { Fields.ActionDate[this] = value; } }
+        public DateTime? ActionDate { get => Fields.ActionDate[this]; set => Fields.ActionDate[this] = value; }
 
         [DisplayName("Table Name"), Size(100), Column("TableName"), NotNull, QuickFilter, QuickSearch, NameProperty]
-        public String EntityTableName { get { return Fields.EntityTableName[this]; } set { Fields.EntityTableName[this] = value; } }
+        public String EntityTableName { get => Fields.EntityTableName[this]; set => Fields.EntityTableName[this] = value; }
 
-        [DisplayName("Entity Id"), NotNull]
-        public String EntityId { get { return Fields.EntityId[this]; } set { Fields.EntityId[this] = value; } }
-        
+        [DisplayName("Entity Id"), NotNull, QuickFilter]
+        public String EntityId { get => Fields.EntityId[this]; set => Fields.EntityId[this] = value; }
+
         [DisplayName("Changes")]
-        public String Changes { get { return Fields.Changes[this]; } set { Fields.Changes[this] = value; } }
+        public String Changes { get => Fields.Changes[this]; set => Fields.Changes[this] = value; }
 
         [DisplayName("IP Address"), Size(100)]
         public String IpAddress { get { return Fields.IpAddress[this]; } set { Fields.IpAddress[this] = value; } }
 
         [DisplayName("Session Id"), Size(100)]
-        public String SessionId { get { return Fields.SessionId[this]; } set { Fields.SessionId[this] = value; } }
+        public String SessionId { get => Fields.SessionId[this]; set => Fields.SessionId[this] = value; }
         
-        #region Foreign Fields
-        #endregion Foreign Fields
-
 
         public class RowFields : RowFieldsBase
         {
             public Int64Field Id;
             public StringField UserId;
-            public Int32Field ActionType;
+            public EnumField<AuditActionType> ActionType;
             public DateTimeField ActionDate;
             public StringField EntityTableName;
             public StringField EntityId;
             public StringField Changes;
             public StringField IpAddress;
             public StringField SessionId;
-
-            public RowFields() : base()
-            {
-                LocalTextPrefix = "_Ext.AuditLog";
-            }
         }
 
         public AuditLogRow() : base(Fields) { }
-
     }
 }
