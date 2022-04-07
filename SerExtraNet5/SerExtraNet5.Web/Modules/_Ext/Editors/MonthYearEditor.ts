de8a7c484b245f1060e830280d4e88891f21a0e5
dfaruque <dfaruque@gmail.com>
2022-04-07 10:09:30 +0600

    usingBootstrapDatePicker as month picker instead on <input type="month"/> for cross browser compatibility

--- SerExtraNet5/SerExtraNet5.Web/Modules/_Ext/Editors/MonthYearEditor.ts
+++ SerExtraNet5/SerExtraNet5.Web/Modules/_Ext/Editors/MonthYearEditor.ts
@@ -1,22 +1,33 @@
 ﻿namespace _Ext {
     @Serenity.Decorators.registerClass([Serenity.IGetEditValue, Serenity.ISetEditValue, Serenity.IReadOnly])
     @Serenity.Decorators.editor()
-    @Serenity.Decorators.element('<input type="month"/>')
+    @Serenity.Decorators.element('<input type="text" autocomplete="off"/>')
     export class MonthYearEditor extends Serenity.Widget<any> implements Serenity.IGetEditValue, Serenity.ISetEditValue, Serenity.IReadOnly {
         public getEditValue(property, target) { target[property.name] = this.value; }
         public setEditValue(source, property) { this.value = source[property.name]; }
 
         constructor(container: JQuery) {
             super(container);
-        }
 
+            usingBootstrapDatePicker();
+
+            (<any>this.element).BSdatepicker({
+                format: "yyyy-mm",
+                startView: "months",
+                minViewMode: "months",
+                language: q.getSelectedLanguage().substr(0, 2),
+                enableOnReadonly: false,
+                showOnFocus: false,
+                autoclose: true
+            });
+        }
 
         public get value(): string {
             return this.element.val();
         }
 
         public set value(val: string) {
-            this.element.val(Q.formatDate(val, 'yyyy-MM'));
+            this.element['BSdatepicker']("update", Q.parseISODateTime(val));
         }
 
         get_readOnly(): boolean {
@@ -25,15 +36,22 @@
 
         set_readOnly(value: boolean): void {
             if (value == true) {
-                this.element.addClass('readonly').attr('readonly', 'readonly');
-                this.element.attr("disabled");
+                this.element.addClass('readonly')
+                    .attr("disabled", "disabled");
             } else {
-                this.element.removeClass('readonly');
-                this.element.removeAttr("readonly");
-                this.element.removeAttr("disabled");
+                this.element.removeClass('readonly')
+                    .removeAttr("disabled");
             }
         }
 
+        //get_minValue(): string { }
+        set_minValue(value: string): void {
+            this.element['BSdatepicker']("setStartDate", Q.parseISODateTime(value));
+        }
+        //get_maxValue(): string { }
+        set_maxValue(value: string): void {
+            this.element['BSdatepicker']("setEndDate", Q.parseISODateTime(value));
+        }
 
     }
 }
