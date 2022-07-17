4fc88c9d41ae955dc2adc84d4d0a3b8950bf8a30
dfaruque <dfaruque@gmail.com>
2022-07-17 16:55:31 +0600

    ExcelImportRequest.Validate

---
+++ SerExtraNet5/SerExtraNet5.Web/Modules/Common/ExcelImporter/ExcelImportRequest.cs
@@ -0,0 +1,30 @@
+﻿using Serenity.Services;
+using Serenity.Web;
+using System;
+using System.Collections.Generic;
+
+namespace SerExtraNet5.Common
+{
+    public class ExcelImportRequest : ServiceRequest
+    {
+        public Int64 ExcelImportTemplateId { get; set; }
+        public String FileName { get; set; }
+
+        public static bool Validate(ExcelImportRequest request, IUploadStorage uploadStorage)
+        {
+            if (request == null)
+                throw new ArgumentNullException(nameof(request));
+
+            if (string.IsNullOrWhiteSpace(request.FileName))
+                throw new ArgumentNullException(nameof(request.FileName));
+
+            if (!uploadStorage.FileExists(request.FileName))
+                throw new ArgumentException("File not found in the server!");
+
+            if (!request.FileName.EndsWith(".xlsx", StringComparison.OrdinalIgnoreCase))
+                throw new ArgumentException("The file type not supported! Please save as the file type as .xlsx");
+
+            return true;
+        }
+    }
+}
