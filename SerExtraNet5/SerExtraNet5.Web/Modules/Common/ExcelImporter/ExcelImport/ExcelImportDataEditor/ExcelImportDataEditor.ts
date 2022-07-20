a7d8baef79c7f426fa4f946ee87097c269f8562d
dfaruque <dfaruque@gmail.com>
2022-07-20 16:21:47 +0600

    updated refCellValue

--- SerExtraNet5/SerExtraNet5.Web/Modules/Common/ExcelImporter/ExcelImport/ExcelImportDataEditor/ExcelImportDataEditor.ts
+++ SerExtraNet5/SerExtraNet5.Web/Modules/Common/ExcelImporter/ExcelImport/ExcelImportDataEditor/ExcelImportDataEditor.ts
@@ -7,6 +7,30 @@ namespace SerExtraNet5.Common {
         constructor(container: JQuery) {
             container.css({ minHeight: 230 });
             super(container);
+
+            this.slickGrid.onCellChange.subscribe((e, args) => {
+                let cell = args.cell;
+                let row = args.row;
+                let grid = args.grid as Slick.Grid;
+                let item = args.item;
+
+                let column = this.allColumns[cell];
+                let cellValue = item[column.field];
+
+                let refColumn = this.allColumns.filter(f => f.sourceItem.editLinkIdField == column.field)[0];
+                let refCellValue = item[refColumn.field];
+
+                let items = this.getItems();
+
+                items.forEach(f => {
+                    if (f[refColumn.field] == refCellValue)
+                        f[column.field] = cellValue;
+                });
+
+                this.setItems(items);
+
+            });
+
         }
 
         protected getButtons(): Serenity.ToolButton[] {
