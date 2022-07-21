5e82088bc2872d95eca072b17d4c1f48e2791838
dfaruque <dfaruque@gmail.com>
2022-07-21 16:54:11 +0600

    excel import master detail data

--- SerExtraNet5/SerExtraNet5.Web/Modules/Common/ExcelImporter/ExcelImport/ExcelImportDialog.ts
+++ SerExtraNet5/SerExtraNet5.Web/Modules/Common/ExcelImporter/ExcelImport/ExcelImportDialog.ts
@@ -34,6 +34,9 @@ namespace SerExtraNet5.Common {
 
                 var saveRequest = this.getSaveRequest();
 
+                let transformedData = ExcelImportDialog.getTransformedDataFlatToMasterDetail(this.form.ImportedData.value);
+                saveRequest.Entity.ImportedData = JSON.stringify(transformedData);
+
                 ExcelImportService.ImportExcelData(saveRequest, response => {
                     Q.notifyInfo('Imported records: ' + (response.Inserted || 0));
 
@@ -46,6 +49,91 @@ namespace SerExtraNet5.Common {
                     }
                 });
             };
+        }
+
+        static getTransformedDataFlatToMasterDetail(flatRecords: any[]): any[] {
+            let firstRecord = flatRecords[0];
+            let flatColumns: string[] = [];
+
+            for (let field in firstRecord) {
+                flatColumns.push(field)
+            }
+
+            let masterDetailModel = {};
+            let detailFields: string[] = []
+
+            for (let columnName of flatColumns) {
+                let dotIndex = columnName.indexOf('.');
+                if (dotIndex < 0) {
+                    masterDetailModel[columnName] = columnName;
+                } else {
+                    let detailField = columnName.substr(0, dotIndex);
+                    if (!masterDetailModel.hasOwnProperty(detailField)) {
+                        masterDetailModel[detailField] = {};
+                        detailFields.push(detailField)
+                    }
+
+                    masterDetailModel[detailField][columnName.substr(dotIndex + 1)] = columnName;
+                }
+            }
+
+            if (detailFields.length == 0) //recursion termination
+                return flatRecords;
+
+            let groupByMasterFields = {};
+
+            let flatRecordsWithMasterKeyGen = flatRecords;
+
+            let masterRecords = [];
+
+            for (let flatRecord of flatRecordsWithMasterKeyGen) {
+                let recordKey = '';
+                let masterRecord = {};
+
+                for (let field in masterDetailModel) {
+                    recordKey += flatRecord[field];
+                    masterRecord[field] = flatRecord[field];
+                }
+
+                flatRecord.recordKey = recordKey;
+                masterRecord['recordKey'] = recordKey;
+
+                if (!groupByMasterFields.hasOwnProperty(recordKey)) {
+                    groupByMasterFields[recordKey] = [];
+                    masterRecords.push(masterRecord);
+                }
+                groupByMasterFields[recordKey].push(flatRecord);
+            }
+
+            let masterDetailRecords = [];
+
+            for (let masterKey in groupByMasterFields) {
+                let masterRecord = masterRecords.filter(f => f.recordKey == masterKey)[0];
+                delete masterRecord['recordKey'];
+                let detailFieldInMaster = detailFields[0];
+
+                let detailRecords = groupByMasterFields[masterKey];
+
+                for (let detailRecord of detailRecords) {
+                    delete detailRecord['recordKey'];
+
+                    for (let masterField in masterDetailModel) {
+                        delete detailRecord[masterField];
+                    }
+
+                    for (let detailFeild in detailRecord) {
+                        let detailFeildWithoutPrefix = detailFeild.substr(detailFieldInMaster.length + 1)
+                        detailRecord[detailFeildWithoutPrefix] = detailRecord[detailFeild];
+                        delete detailRecord[detailFeild];
+                    }
+                }
+
+                masterRecord[detailFieldInMaster] = ExcelImportDialog.getTransformedDataFlatToMasterDetail(detailRecords);
+
+                masterDetailRecords.push(masterRecord);
+            }
+
+            return masterDetailRecords;
         }
 
         private setExcelDataGridColumns() {
--- SerExtraNet5/SerExtraNet5.Web/Modules/Common/ExcelImporter/ExcelImportableTableLookup.cs
+++ SerExtraNet5/SerExtraNet5.Web/Modules/Common/ExcelImporter/ExcelImportableTableLookup.cs
@@ -17,6 +17,7 @@ namespace SerExtraNet5.Common
     {
         public static List<ExcelImportableTable> Items = new();
 
+        private const string MasterAndDetailFieldSeparator = ".";
         public IPropertyItemProvider PropertyProvider { get; }
         public IServiceProvider ServiceProvider { get; }
 
@@ -41,44 +42,7 @@ namespace SerExtraNet5.Common
 
                 if (tableNameAttr is null) continue;
 
-                var propertyItems = PropertyProvider.GetPropertyItemsFor(rowType);
-
-                var excelImportableFields = new List<PropertyItem>();
-
-                var rowProps = rowType.GetProperties();
-                foreach (var propertyItem in propertyItems)
-                {
-                    var prop = rowType.GetProperty(propertyItem.Name);
-                    var excelImportableAttr = prop.GetAttribute<ExcelImportableAttribute>();
-                    if (excelImportableAttr is null) continue;
-
-                    var row = Activator.CreateInstance(rowType) as IRow;
-                    var field = row.FindField(propertyItem.Name);
-
-                    if (field.Flags.HasFlag(FieldFlags.NotNull)) //NotNull fiels not marked as Required! why?
-                        propertyItem.Required = true;
-
-                    excelImportableFields.Add(propertyItem);
-
-                    //add ForeignIdField if this property is a ForeignField
-                    if (field.Flags.HasFlag(FieldFlags.Foreign))
-                    {
-                        var foreignIdField = row.Fields.FirstOrDefault(f => f.TextualField == field.Name);
-                        if (foreignIdField.GetAttribute<ExcelImportableAttribute>() is null)
-                        {
-                            propertyItem.EditLinkIdField = foreignIdField.Name;
-                            propertyItem.Title = propertyItem.Title + " [" + propertyItem.Name + "]";
-
-                            var foreignPropertyItem = propertyItems.First(f => f.Name == foreignIdField.Name);
-                            foreignPropertyItem.Title = foreignPropertyItem.Title + " [" + foreignPropertyItem.Name + "]";
-                            
-                            if (foreignIdField.Flags.HasFlag(FieldFlags.NotNull))
-                                foreignPropertyItem.Required = true;
-
-                            excelImportableFields.Add(foreignPropertyItem);
-                        }
-                    }
-                }
+                var excelImportableFields = GetImportableFields(rowType);
 
                 Items.Add(new ExcelImportableTable
                 {
@@ -89,6 +53,63 @@ namespace SerExtraNet5.Common
             }
 
             return Items;
+        }
+
+        private List<PropertyItem> GetImportableFields(Type rowType, string sourceFieldPrefix = "")
+        {
+            var propertyItems = PropertyProvider.GetPropertyItemsFor(rowType);
+
+            var excelImportableFields = new List<PropertyItem>();
+
+            var rowProps = rowType.GetProperties();
+            foreach (var propertyItem in propertyItems)
+            {
+                var prop = rowType.GetProperty(propertyItem.Name);
+                var excelImportableAttr = prop.GetAttribute<ExcelImportableAttribute>();
+                if (excelImportableAttr is null) continue;
+
+                var row = Activator.CreateInstance(rowType) as IRow;
+                var field = row.FindField(propertyItem.Name);
+
+                if (field.Flags.HasFlag(FieldFlags.NotNull)) //NotNull fiels not marked as Required! why?
+                    propertyItem.Required = true;
+
+                if (prop.GetAttribute<MasterDetailRelationAttribute>() is MasterDetailRelationAttribute masterDetailRelation)
+                {
+                    var detailRowType = prop.PropertyType.GenericTypeArguments.FirstOrDefault();
+                    sourceFieldPrefix = sourceFieldPrefix + prop.Name + MasterAndDetailFieldSeparator;
+                    var detailImportableFields = GetImportableFields(detailRowType, sourceFieldPrefix);
+                    detailImportableFields.ForEach(f =>
+                    {
+                        f.Name = sourceFieldPrefix + f.Name;
+                        f.Title = sourceFieldPrefix + f.Title;
+                    });
+                    excelImportableFields.AddRange(detailImportableFields);
+                }
+                else
+                    excelImportableFields.Add(propertyItem);
+
+                //add ForeignIdField if this property is a ForeignField
+                if (field.Flags.HasFlag(FieldFlags.Foreign))
+                {
+                    var foreignIdField = row.Fields.FirstOrDefault(f => f.TextualField == field.Name);
+                    if (foreignIdField.GetAttribute<ExcelImportableAttribute>() is null)
+                    {
+                        propertyItem.EditLinkIdField = sourceFieldPrefix + foreignIdField.Name;
+                        propertyItem.Title = propertyItem.Title + " [" + propertyItem.Name + "]";
+
+                        var foreignPropertyItem = propertyItems.First(f => f.Name == foreignIdField.Name);
+                        foreignPropertyItem.Title = foreignPropertyItem.Title + " [" + foreignPropertyItem.Name + "]";
+
+                        if (foreignIdField.Flags.HasFlag(FieldFlags.NotNull))
+                            foreignPropertyItem.Required = true;
+
+                        excelImportableFields.Add(foreignPropertyItem);
+                    }
+                }
+            }
+
+            return excelImportableFields;
         }
     }
 }
