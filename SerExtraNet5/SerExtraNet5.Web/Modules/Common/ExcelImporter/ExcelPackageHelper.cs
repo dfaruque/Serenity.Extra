4facd8c42644046ead934c944427f0617ba75f7e
dfaruque <dfaruque@gmail.com>
2022-07-24 18:43:39 +0600

    fixed empty sheet error

--- SerExtraNet5/SerExtraNet5.Web/Modules/Common/ExcelImporter/ExcelPackageHelper.cs
+++ SerExtraNet5/SerExtraNet5.Web/Modules/Common/ExcelImporter/ExcelPackageHelper.cs
@@ -10,7 +10,7 @@ namespace _Ext.ExcelImporter
         {
             var columns = new List<string>();
 
-            for (var column = 1; column <= worksheet.Dimension.End.Column; column++)
+            for (var column = 1; column <= worksheet.Dimension?.End?.Column; column++)
             {
                 var columnName = Convert.ToString(worksheet.Cells[1, column].Value);
                 columns.Add(columnName);
