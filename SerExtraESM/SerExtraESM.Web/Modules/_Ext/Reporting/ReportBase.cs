3d2cbd2edd5222fb581b1b0f6827441dfa81ff22
dfaruque <dfaruque@gmail.com>
2023-08-29 14:45:10 +0600

    code formatting

--- SerExtraESM/SerExtraESM.Web/Modules/_Ext/Reporting/ReportBase.cs
+++ SerExtraESM/SerExtraESM.Web/Modules/_Ext/Reporting/ReportBase.cs
@@ -1,57 +1,52 @@
-﻿using Serenity;
-using Serenity.Data;
+﻿using Serenity.Data;
 using Serenity.Reporting;
 using Serenity.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
-namespace _Ext
+namespace _Ext;
+
+public partial class ListReportBase : ReportBase
 {
-    public partial class ListReportBase : ReportBase
+    public ListReportRequest Request { get; set; }
+
+    public ListReportBase(IRequestContext requestContext, ISqlConnections sqlConnections) : base(requestContext, sqlConnections)
     {
-        public ListReportRequest Request { get; set; }
 
-        public ListReportBase(IRequestContext requestContext, ISqlConnections sqlConnections) : base(requestContext, sqlConnections)
-        {
+    }
+}
 
-        }
+public partial class EntityReportBase : ReportBase
+{
+    public EntityReportRequest Request { get; set; }
+
+    public EntityReportBase(IRequestContext requestContext, ISqlConnections sqlConnections) : base(requestContext, sqlConnections)
+    {
+
+    }
+}
+
+public abstract class ReportBase : BaseRepository, ICustomizeHtmlToPdf
+{
+    protected ISqlConnections SqlConnections { get; private set; }
+    protected IRequestContext RequestContext { get; private set; }
+    public ReportBase(IRequestContext requestContext, ISqlConnections sqlConnections) : base(requestContext)
+    {
+        SqlConnections = sqlConnections;
+        RequestContext = requestContext;
     }
 
-    public partial class EntityReportBase : ReportBase
+    public virtual void Customize(IHtmlToPdfOptions options)
     {
-        public EntityReportRequest Request { get; set; }
-
-        public EntityReportBase(IRequestContext requestContext, ISqlConnections sqlConnections) : base(requestContext, sqlConnections)
-        {
-
-        }
+        // you may customize HTML to PDF converter (WKHTML) parameters here, e.g. 
+        options.Landscape = false;
+        options.PageSize = "A4";
+        options.MarginsAll = "1.2cm";
+        options.FooterHtmlUrl = GetBaseUrl() + "Modules/_Ext/Reporting/ReportFooter.html";
+        options.SmartShrinking = true;
     }
 
-    public abstract class ReportBase : BaseRepository, ICustomizeHtmlToPdf
+    public string GetBaseUrl()
     {
-        protected ISqlConnections SqlConnections { get; private set; }
-        protected IRequestContext RequestContext { get; private set; }
-        public ReportBase(IRequestContext requestContext, ISqlConnections sqlConnections) : base(requestContext)
-        {
-            SqlConnections = sqlConnections;
-            RequestContext = requestContext;
-        }
-
-        public virtual void Customize(IHtmlToPdfOptions options)
-        {
-            // you may customize HTML to PDF converter (WKHTML) parameters here, e.g. 
-            options.Landscape = false;
-            options.PageSize = "A4";
-            options.MarginsAll = "1.2cm";
-            options.FooterHtmlUrl = GetBaseUrl() + "Modules/_Ext/Reporting/ReportFooter.html";
-            options.SmartShrinking = true;
-        }
-
-        public string GetBaseUrl()
-        {
-            var baseUrl = string.Empty;
+        var baseUrl = string.Empty;
 //#if COREFX
 
 //#else
@@ -64,8 +59,7 @@ namespace _Ext
 //            baseUrl = string.Format("{0}://{1}{2}", httpRequest.Url.Scheme, httpRequest.Url.Authority, appUrl);
 //#endif
 
-            return baseUrl;
-        }
-
+        return baseUrl;
     }
+
 }
--- SerExtraESM/SerExtraESM.Web/Modules/_Ext/Reporting/ReportRequest.cs
+++ SerExtraESM/SerExtraESM.Web/Modules/_Ext/Reporting/ReportRequest.cs
@@ -1,45 +1,39 @@
-﻿using Serenity.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using Serenity.Services;
 
-namespace _Ext
+namespace _Ext;
+
+public partial class ListReportRequest : ListRequest
 {
-    public partial class ListReportRequest : ListRequest
+    /// <summary>
+    /// it is only used in client side to determine report key for generating report
+    /// </summary>
+    public string ReportKey { get; set; }
+    /// <summary>
+    /// it is only used in client side to determine endpoint method name
+    /// </summary>
+    public string ReportServiceMethodName { get; set; } //"Report";
+    public string ListExcelServiceMethodName { get; set; } //"ListExcel";
+    public string ReportDesignPath { get; set; } //"~/Modules/.../.../Report/...ReportDesign.cshtml";
+
+    public Dictionary<string, string> EqualityFilterWithTextValue { get; set; }
+    public Dictionary<string, object> CustomParameters { get; set; }
+}
+
+public partial class EntityReportRequest : RetrieveRequest
+{
+    /// <summary>
+    /// it is only used in client side to determine report key for generating report
+    /// </summary>
+    public string ReportKey { get; set; }
+    /// <summary>
+    /// it is only used in client side to determine endpoint method name
+    /// </summary>
+    public string ReportServiceMethodName { get; set; } //"Report";
+    public string ReportDesignPath { get; set; } //"~/Modules/.../.../Report/...ReportDesign.cshtml";
+
+    public EntityReportRequest()
     {
-        /// <summary>
-        /// it is only used in client side to determine report key for generating report
-        /// </summary>
-        public string ReportKey { get; set; }
-        /// <summary>
-        /// it is only used in client side to determine endpoing method name
-        /// </summary>
-        public string ReportServiceMethodName { get; set; } //"Report";
-        public string ListExcelServiceMethodName { get; set; } //"ListExcel";
-        public string ReportDesignPath { get; set; } //"~/Modules/.../.../Report/...ReportDesign.cshtml";
-
-        public Dictionary<string, string> EqualityFilterWithTextValue { get; set; }
-        public Dictionary<string, object> CustomParameters { get; set; }
-    }
-
-    public partial class EntityReportRequest : RetrieveRequest
-    {
-        /// <summary>
-        /// it is only used in client side to determine report key for generating report
-        /// </summary>
-        public string ReportKey { get; set; }
-        /// <summary>
-        /// it is only used in client side to determine endpoing method name
-        /// </summary>
-        public string ReportServiceMethodName { get; set; } //"Report";
-        public string ReportDesignPath { get; set; } //"~/Modules/.../.../Report/...ReportDesign.cshtml";
-
-        public EntityReportRequest()
-        {
-            ColumnSelection = RetrieveColumnSelection.Details;
-
-        }
-
+        ColumnSelection = RetrieveColumnSelection.Details;
     }
 }
