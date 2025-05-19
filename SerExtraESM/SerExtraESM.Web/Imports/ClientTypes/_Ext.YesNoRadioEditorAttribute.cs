bc7d618ddc6b2b39756d91a8efffacc8d86f70e9
dfaruque <dfaruque@gmail.com>
2025-05-19 14:10:17 +0600

    YesNoRadioEditorAttribute

---
+++ SerExtraESM/SerExtraESM.Web/Imports/ClientTypes/_Ext.YesNoRadioEditorAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Serenity;
+using Serenity.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace _Ext;
+
+public partial class YesNoRadioEditorAttribute : CustomEditorAttribute
+{
+    public const string Key = "_Ext.YesNoRadioEditor";
+
+    public YesNoRadioEditorAttribute()
+        : base(Key)
+    {
+    }
+}
